952941c308dbcafd03abbc5709eaadde62c18341
codcodea <fredrik.halsius@gmail.com>
2023-11-22 13:11:14 +0100

    responsive elements

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,27 +1,32 @@
-
+const backdrop = document.querySelector("main");
+const dialog = document.querySelector("dialog");
+const image = document.querySelector(".image");
+const figcaption = document.querySelector("figcaption");
 
 const showGallery = () => {
-    const dialog = document.querySelector('dialog')
-    const main = document.querySelector('main')
-    dialog.addEventListener('click', () => {
-        dialog.close()
-        main.style.filter = 'none'
-    
-    })
-    main.style.filter = 'blur(1px) brightness(0.7)'
-    dialog.showModal()
-}
+	dialog.addEventListener("click", closeGallery);
+	backdrop.style.filter = "blur(1px) brightness(0.7)";
+	dialog.showModal();
+};
 
-const addEvent = (img) => {
-    img.addEventListener('click', (e) => {
-        const popup = document.querySelector('.image')
-        const figcaption = document.querySelector('figcaption')
-        popup.src = e.target.src
-        figcaption.textContent = e.target.alt
-        showGallery()
-    })
-}
+const closeGallery = () => {
+	dialog.close();
+	backdrop.style.filter = "none";
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.gallery-item').forEach(img => addEvent(img)) 
-})
+const keydown = (e) => {
+	if (e.key === "Escape") closeGallery();
+};
+
+const addEvents = (img) => {
+	img.addEventListener("click", (e) => {
+		image.src = e.target.src;
+		figcaption.textContent = e.target.alt;
+		showGallery();
+	});
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelectorAll(".gallery-item").forEach((img) => addEvents(img));
+	document.addEventListener("keydown", keydown);
+});
