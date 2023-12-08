c23189161124bd8846e4c1518f0a2103699e5e99
codcodea <fredrik.halsius@gmail.com>
2023-12-08 17:51:13 +0100

    js for safari fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 import "../html/components/toolbar/toolbar.js";
 import "../html/components/footer/footer.js";
+
+// safari fix
+document.addEventListener("DOMContentLoaded", () => {
+    if(customElements.get("my-toolbar") || customElements.get("my-footer")) {
+        document.querySelector("main").style.display = "block";
+    }
+});
