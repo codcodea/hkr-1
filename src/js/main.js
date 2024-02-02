78fb443cceae1eab0387364c0088d7735b8e24bb
codcodea <fredrik.halsius@gmail.com>
2024-02-02 22:11:21 +0100

    laptop

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import "../html/components/toolbar/toolbar.js";
 import "../html/components/footer/footer.js";
 
-// safari fix
 document.addEventListener("DOMContentLoaded", () => {
     if(customElements.get("my-toolbar") || customElements.get("my-footer")) {
         document.querySelector("main").style.display = "block";
